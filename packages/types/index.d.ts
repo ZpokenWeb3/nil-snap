db456765ccd18849feec1b6b25ab7bccdd61a824
Vadim <eugene.s@zpoken.ai>
2024-08-18 16:51:48 +0300

    add general types

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,5 +1,31 @@
 export type Address = `0x${string}`;
 
-export type Account = { account: Address; isDeployed: boolean };
+export type Account = {
+  address: Address;
+  isDeployed: boolean;
+  shardId: number;
+};
 
 export type Currency = { id: string; balance: string };
+
+export type SendRequestParams = {
+  receiver: Address;
+  amount: string;
+  tokenId: number;
+};
+
+export type GetBalanceRequestParams = {
+  userAddress: Address;
+};
+
+export enum NIL_METHODS {
+  CREATE_ACCOUNT = 'nil_createAccount',
+  DEPLOY_ACCOUNT = 'nil_deployAccount',
+  GET_BALANCE = 'nil_getBalance',
+  FAUCET = 'nil_faucet',
+  GET_CURRENCIES = 'nil_getCurrencies',
+  CREATE_AND_MINT = 'nil_createAndMint',
+  SEND = 'nil_send',
+}
+
+export type ApiRequestParams = GetBalanceRequestParams | SendRequestParams;
